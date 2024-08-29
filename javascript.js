9a385be67f409e5e5551f76e1d978216711d4f3b
lieri123 <142183090+lieri123@users.noreply.github.com>
2024-08-28 20:05:16 -0400

    Change the return type of getComputerChoice to a string rock, paper, or scissors

--- javascript.js
+++ javascript.js
@@ -1,10 +1,21 @@
 
-let compChoice = 0; 
-let humanChoice = 0; 
+let compChoice = ""; 
+let humanChoice = ""; 
+let humanScore = 0; 
+let computerScore = 0; 
 
 function getComputerChoice(){
 
-    return Math.floor(Math.random()*3); 
+    let symbol = Math.floor(Math.random()*3);
+
+    if(symbol == 0){
+        return 'rock'; 
+    }
+    else if(symbol == 1){
+        return 'paper'; 
+    }
+
+    return  'scissors'
 
 }
 
